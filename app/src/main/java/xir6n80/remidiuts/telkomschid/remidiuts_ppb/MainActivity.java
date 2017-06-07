46f109ba655d81a6fcdfb030f15aa6d33d659390
daniamri <daniamri93@gmail.com>
2017-06-07 21:12:50 +0700

    Action Bar Spinner

--- app/src/main/java/xir6n80/remidiuts/telkomschid/remidiuts_ppb/MainActivity.java
+++ app/src/main/java/xir6n80/remidiuts/telkomschid/remidiuts_ppb/MainActivity.java
@@ -24,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ActionTabsActivity.class));
             }
         });
+        findViewById(R.id.buttonNav3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ActionSpinnerActivity.class));
+            }
+        });
 
 
     }
