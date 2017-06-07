f0ecee8e825c9446abdff26fed3d6b523bc80848
daniamri <daniamri93@gmail.com>
2017-06-07 20:55:18 +0700

    Swipe View

--- app/src/main/java/xir6n80/remidiuts/telkomschid/remidiuts_ppb/MainActivity.java
+++ app/src/main/java/xir6n80/remidiuts/telkomschid/remidiuts_ppb/MainActivity.java
@@ -1,7 +1,9 @@
 package xir6n80.remidiuts.telkomschid.remidiuts_ppb;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonNav1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SwiperViewActivity.class));
+            }
+        });
 
 
     }
